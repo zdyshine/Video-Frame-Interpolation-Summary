b09e98dafef544701fa6bda2691b9be7c4bf7eb4
PixelArtAI <35593072+zdyshine@users.noreply.github.com>
2024-10-10 10:49:01 +0800

    Update ffmpeg_utils.py

--- basic_infer/ffmpeg_utils.py
+++ basic_infer/ffmpeg_utils.py
@@ -221,5 +221,7 @@ writer = (ffmpeg
             .global_args('-hide_banner').global_args('-loglevel', 'quiet')
             .overwrite_output()
             .run_async(pipe_stdin=True, cmd=ffmpeg_bin))
-writer.stdin.write(np.ascontiguousarray(frame).tobytes())      
+writer.stdin.write(np.ascontiguousarray(frame).tobytes())   
+writer.stdin.close()
+writer.wait()
 '''
